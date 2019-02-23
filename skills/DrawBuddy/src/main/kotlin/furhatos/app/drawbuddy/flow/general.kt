41aa880808efdeb39738b2e74c224840b92cd451
stefannygard <stefan_nygard@hotmail.com>
2019-02-23 13:21:17 +0100

    initial commit :tada:

---
+++ skills/DrawBuddy/src/main/kotlin/furhatos/app/drawbuddy/flow/general.kt
@@ -0,0 +1,58 @@
+package furhatos.app.drawbuddy.flow
+import furhatos.app.drawbuddy.PORT
+import furhatos.event.senses.SenseSkillGUIConnected
+import furhatos.flow.kotlin.*
+import furhatos.skills.HostedGUI
+import furhatos.util.*
+
+// Our GUI declaration
+val GUI = HostedGUI("ExampleGUI", "assets/exampleGui", PORT)
+
+val NoGUI : State = state(null) {
+
+    onEvent<SenseSkillGUIConnected> {
+        goto(Idle)
+    }
+
+}
+
+val Idle: State = state {
+
+    init {
+        furhat.setVoice(Language.ENGLISH_US, Gender.MALE)
+        if (users.count > 0) {
+            furhat.attend(users.random)
+            goto(Start)
+        }
+    }
+
+    onEntry {
+        furhat.attendNobody()
+    }
+
+    onUserEnter {
+        furhat.attend(it)
+        goto(Start)
+    }
+}
+
+val Interaction: State = state {
+
+    onUserLeave(instant = true) {
+        if (users.count > 0) {
+            if (it == users.current) {
+                furhat.attend(users.other)
+                goto(Start)
+            } else {
+                furhat.glance(it)
+            }
+        } else {
+            goto(Idle)
+        }
+    }
+
+    onUserEnter(instant = true) {
+        furhat.glance(it)
+    }
+
+}
---
+++ skills/DrawBuddy/src/main/kotlin/furhatos/app/drawbuddy/flow/interaction.kt
@@ -0,0 +1,22 @@
+package furhatos.app.drawbuddy.flow
+import furhatos.nlu.common.*
+import furhatos.flow.kotlin.*
+import furhatos.app.drawbuddy.*
+import furhatos.app.drawbuddy.nlu.*
+import furhatos.skills.HostedGUI
+
+// start conversation
+val Start = state {
+
+    onEntry {
+        random(
+                { furhat.ask("Hej där! Vill du leka?") },
+                { furhat.ask("Nämen hejsan! Vad sägs om att pyssla lite?") }
+        )
+    }
+
+    /*onResponse<No> {
+        furhat.say("Jag förstår, ha en bra dag!")
+        //goto(Idle)
+    }*/
+}
